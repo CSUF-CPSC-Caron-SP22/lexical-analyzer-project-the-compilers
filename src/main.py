81d1c2e8e41360172ca64626f9a39dc0b746d75e
Robert Pace <rpaceiv@.com>
2022-03-09 22:21:11 -0800

    Added arg handler and a preprocessor.py file

--- src/main.py
+++ src/main.py
@@ -1,2 +1,25 @@
 import sys
+import pandas as pd
+
+#argv 1 source code
+#argv 2 reserved_words
+#argv 3 scanning 
+#argv 4 token_table
+
+
+if len(sys.argv) == 5:
+    source_code = sys.argv[1]
+    reserved_df = pd.read_csv(sys.argv[2])
+    scanning_df = pd.read_csv(sys.argv[3])
+    token_df = pd.read_csv(sys.argv[4])
+elif len(sys.argv) == 2:
+    source_code = sys.argv[1]
+    reserved_df = pd.read_csv('../tables/reserved_words.csv')
+    scanning_df = pd.read_csv('../tables/scanning_table.csv')
+    token_df = pd.read_csv('../tables/token_table.csv')
+else:
+    print("ERROR")
+    exit(0)
+
+
 
