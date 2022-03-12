f73eef1eb9b660a9fb32e8d6dd64a72bb3f791ca
Jacob Ursenbach <61986930+jlursenbach@users.noreply.github.com>
2022-03-11 17:23:49 -0800

    source code chages
    updated source_code to take optional empty string

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -1,7 +1,8 @@
 # from pandas import pd
 
 class Preprocessor:
-    def __init__(self,source_code_file,reserved_words_df,token_table_df,scanning_table_df):
+    def __init__(self,reserved_words_df,token_table_df,scanning_table_df,source_code_file: str = ""):
+
         self.source_code = self.__fileToStr(source_code_file)
         self.reserved_words = self.__dFToList(reserved_words_df)
         self.token_table = self.__dFToDict(token_table_df)
@@ -9,9 +10,12 @@ class Preprocessor:
 
     '''This function takes in the source code and returns a string'''
     def __fileToStr(self, source_code_file) -> str:
-        file = open(source_code_file, "r")
-        return file.read()
-    
+        if source_code_file != '':
+            file = open(source_code_file, "r")
+            return file.read()
+        else:
+            return ""
+            
     '''This function takes in a pandas data frame and returns a list'''
     def __dFToList(self,reserved_words_df) -> list:
         return reserved_words_df[reserved_words_df.columns[0]].tolist()
